9e797e9cfbb5a8cb35b21198b5f429390cc659ab
Francis Mendez <fmendez884@gmail.com>
2019-03-29 05:01:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,15 @@
 # Write your code here.
 
+$count = 0
+
 def line(katz_deli)
   deli = []
     i = 0
   while (i < katz_deli.length)
     deli << "#{i+1}. #{katz_deli[i]}"
-    i +=1 
+   i += 1
   end
-  
+ 
   if katz_deli.length == 0 
     puts "The line is currently empty."
   else
@@ -15,11 +17,17 @@ def line(katz_deli)
   end
 end
 
-def take_a_number(katz_deli, name)
-  katz_deli << name
-  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
-end
+# def take_a_number(katz_deli, name)
+#   katz_deli << name
+#   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+# end
 
+def take_a_number(katz_deli)
+  $count += 1
+  katz_deli << $count
+  puts "Welcome, you are ticket number #{$count}."
+end
+ 
 def now_serving(katz_deli)
   if katz_deli.length == 0 
     puts "There is nobody waiting to be served!"
@@ -27,3 +35,6 @@ def now_serving(katz_deli)
     puts "Currently serving #{katz_deli.shift}."
   end
 end
+
+
+
